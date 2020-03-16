dbad248093d0e6b033702d40a9c61b82b95bf630
cnxiahuajie <cnxiahuajie@icloud.com>
2020-03-16 16:48:13 +0800

    提交服务注册中心

---
+++ eureka-server/src/test/java/com/vteam/vtarm/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vteam.vtarm.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
